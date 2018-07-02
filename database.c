50f5faae992d9f771a92f65ece9dfcea46d8531a
Jarred Parr <jparr721@gmail.com>
2018-07-02 18:46:11 -0400

    added initializers

--- database.c
+++ database.c
@@ -6,3 +6,15 @@
 #include <unistd.h>
 #include <sys/stat.h>
 
+/* Global reference to the me node. The databaseNode
+ * will always point to this one*/
+databaseNode *me = NULL;
+
+databaseNode *createDatabaseNode(char* nodename);
+int databaseAddNode(databaseNode *node);
+int databasePing();
+int loadConfig(char* filename);
+
+/*----------------------------------------------
+ * Initializers
+ * -------------------------------------------*/
