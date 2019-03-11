046a043b1276c8a033bf87f689b7587d18ddbc2e
Jarred Parr <jparr721@gmail.com>
2019-03-11 09:57:57 -0400

    finally works

--- jupiter/src/api/routes/hello.ts
+++ jupiter/src/api/routes/hello.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/works', (req, res) => {
   res.status(200).send('Hello world from https!');
 });
 
+router.get('/breaks', (req, res) => {
+  res.status(200).send('Should break');
+});
 export default router;
